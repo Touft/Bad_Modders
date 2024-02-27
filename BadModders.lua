010ae6113903507984166c7709538d1ca8e09efa
Touft <99562774+Touft@users.noreply.github.com>
2024-02-28 00:03:30 +0100

    1.55

--- BadModders.lua
+++ BadModders.lua
@@ -1,32 +1,40 @@
-util.require_natives("3095a")
 -- Auto Updater from https://github.com/hexarobi/stand-lua-auto-updater
 local status, auto_updater = pcall(require, "auto-updater")
 if not status then
-    local auto_update_complete = nil util.toast("Installing auto-updater...", TOAST_ALL)
-    async_http.init("raw.githubusercontent.com", "/hexarobi/stand-lua-auto-updater/main/auto-updater.lua",
-        function(result, headers, status_code)
-            local function parse_auto_update_result(result, headers, status_code)
-                local error_prefix = "Error downloading auto-updater: "
-                if status_code ~= 200 then util.toast(error_prefix..status_code, TOAST_ALL) return false end
-                if not result or result == "" then util.toast(error_prefix.."Found empty file.", TOAST_ALL) return false end
-                filesystem.mkdir(filesystem.scripts_dir() .. "lib")
-                local file = io.open(filesystem.scripts_dir() .. "lib\\auto-updater.lua", "wb")
-                if file == nil then util.toast(error_prefix.."Could not open file for writing.", TOAST_ALL) return false end
-                file:write(result) file:close() util.toast("Successfully installed auto-updater lib", TOAST_ALL) return true
-            end
-            auto_update_complete = parse_auto_update_result(result, headers, status_code)
-        end, function() util.toast("Error downloading auto-updater lib. Update failed to download.", TOAST_ALL) end)
-    async_http.dispatch() local i = 1 while (auto_update_complete == nil and i < 40) do util.yield(250) i = i + 1 end
-    if auto_update_complete == nil then error("Error downloading auto-updater lib. HTTP Request timeout") end
-    auto_updater = require("auto-updater")
+    if not async_http.have_access() then
+        util.toast("Failed to install auto-updater. Internet access is disabled. To enable automatic updates, please stop the script then uncheck the `Disable Internet Access` option.")
+    else
+        local auto_update_complete = nil util.toast("Installing auto-updater...", TOAST_ALL)
+        async_http.init("raw.githubusercontent.com", "/hexarobi/stand-lua-auto-updater/main/auto-updater.lua",
+                function(raw_result, raw_headers, raw_status_code)
+                    local function parse_auto_update_result(result, headers, status_code)
+                        local error_prefix = "Error downloading auto-updater: "
+                        if status_code ~= 200 then util.toast(error_prefix..status_code, TOAST_ALL) return false end
+                        if not result or result == "" then util.toast(error_prefix.."Found empty file.", TOAST_ALL) return false end
+                        filesystem.mkdir(filesystem.scripts_dir() .. "lib")
+                        local file = io.open(filesystem.scripts_dir() .. "lib\\auto-updater.lua", "wb")
+                        if file == nil then util.toast(error_prefix.."Could not open file for writing.", TOAST_ALL) return false end
+                        file:write(result) file:close() util.toast("Successfully installed auto-updater lib", TOAST_ALL) return true
+                    end
+                    auto_update_complete = parse_auto_update_result(raw_result, raw_headers, raw_status_code)
+                end, function() util.toast("Error downloading auto-updater lib. Update failed to download.", TOAST_ALL) end)
+        async_http.dispatch() local i = 1 while (auto_update_complete == nil and i < 40) do util.yield(250) i = i + 1 end
+        if auto_update_complete == nil then error("Error downloading auto-updater lib. HTTP Request timeout") end
+        auto_updater = require("auto-updater")
+    end
 end
 if auto_updater == true then error("Invalid auto-updater lib. Please delete your Stand/Lua Scripts/lib/auto-updater.lua and try again") end
 
-auto_updater.run_auto_update({
+---
+--- Auto Updater
+---
+
+local auto_update_config = {
     source_url="https://raw.githubusercontent.com/Touft/Bad_Modders/main/BadModders.lua",
     script_relpath=SCRIPT_RELPATH,
-    verify_file_begins_with="--"
-})
+    verify_file_begins_with="--",
+}
+auto_updater.run_auto_update(auto_update_config)
 --=========================================UPDATES==============================================--
 
 ----------------------------------------------------------------
@@ -88,13 +96,6 @@ for _, playerId in ipairs(playersList) do
 end
 
 ---------------------------------------------------------------
-
--- Manually check for updates with a menu option
-menu.action(menu.my_root(), "Check for Update", {}, "The script will automatically check for updates at most daily, but you can manually check using this option anytime.", function()
-    auto_update_config.check_interval = 0
-    util.toast("Checking for updates")
-    auto_updater.run_auto_update(auto_update_config)
-end)
 
 menu.divider(menu.my_root(), "")
 local modders = menu.list(menu.my_root(), "Bad Modders", {}, "")
